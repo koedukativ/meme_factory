07f03df908861e300a8a3097a2a69dd657d6819a
Joschka Cesar <cesar@posteo.de>
2020-09-02 16:10:23 +0200

    Add Drag Feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
   const loadMemes = () => {
     fetch('https://api.imgflip.com/get_memes')
     .then((response) => response.json())
-    .then((response) => changeMeme(response.data.memes[Math.floor(Math.random() * 100)]));
+    .then((response) => changeMeme(response.data.memes[Math.floor(Math.random() * 100)]))
+    .then(() => {dragEvents()});
   }
   
   const changeText = (num) => {
@@ -18,6 +19,60 @@ function App() {
     editText(textCopy);
   }
 
+  const addMeme = () => {
+    const newMeme = {};
+    newMeme.url = URL.createObjectURL(document.querySelector("#input").files[0]);
+    console.log(newMeme);
+    changeMeme(newMeme);
+  }
+
+  const dragEvents = () => {
+    const firstText = document.querySelectorAll(".memeText")[0];
+    const secondText = document.querySelectorAll(".memeText")[1];
+    dragElement(firstText);
+    dragElement(secondText);
+  }
+
+  const dragElement = (elmnt) => {
+    const clicker = (e) => {
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      console.log(pos1 + " " + pos2 + " " + pos3 + " " + pos4);
+      console.log(elmnt.offsetTop);
+      console.log(elmnt.style.top);
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+
+    const closeDragElement = () => {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+
+    const elementDrag = (e) => {
+      console.log(pos1 + " " + pos2 + " " + pos3 + " " + pos4);
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      console.log(pos1 + " " + pos2 + " " + pos3 + " " + pos4);
+      console.log(elmnt.offsetTop);
+      elmnt.style.top = (elmnt.offsetTop - pos2 - 30) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = clicker;
+  }
+
+
+  
+  
+  
+
+  
+  
+  
 
   return (
     <div className="App">
@@ -26,13 +81,14 @@ function App() {
           <p className="memeText">{memeText[0]}</p>
           <p className="memeText" id="textBottom">{memeText[1]}</p>
           <img id="memePic" src={meme.url}/>
-          <input onChange={() => {changeText(0)}}></input>
-          <input onChange={() => {changeText(1)}}></input>
         </div>
       : 
       null
       }
+      <input onChange={() => {changeText(0)}}></input>
+      <input onChange={() => {changeText(1)}}></input>
       <button onClick={loadMemes}>Load fresh meme!</button>
+      <input onChange={addMeme} type="file" id="input" multiple></input>
     </div>
   );
 }
