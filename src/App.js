5a4c86a8fd24dc83a032ebab6141604ede59e6d1
Joschka Cesar <cesar@posteo.de>
2020-09-02 11:20:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [meme, changeMeme] = useState();
+  const [memeText, editText] = useState(["mememe", "youyuuy"]);
+
+  const loadMemes = () => {
+    fetch('https://api.imgflip.com/get_memes')
+    .then((response) => response.json())
+    .then((response) => changeMeme(response.data.memes[Math.floor(Math.random() * 100)]));
+  }
+  
+  const changeText = (num) => {
+    const textCopy = [...memeText];
+    textCopy[num] = document.querySelectorAll("input")[num].value;
+    editText(textCopy);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {meme ? 
+        <div className="memeContainer">
+          <p className="memeText">{memeText[0]}</p>
+          <p className="memeText" id="textBottom">{memeText[1]}</p>
+          <img id="memePic" src={meme.url}/>
+          <input onChange={() => {changeText(0)}}></input>
+          <input onChange={() => {changeText(1)}}></input>
+        </div>
+      : 
+      null
+      }
+      <button onClick={loadMemes}>Load fresh meme!</button>
     </div>
   );
 }
