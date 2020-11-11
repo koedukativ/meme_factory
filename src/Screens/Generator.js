bfb5465d0f705e37484540eea09263fe9a8ed8d7
Joschka Cesar <cesar@posteo.de>
2020-11-11 14:35:04 +0100

    Bugfix: Meme download was cropped wrongly

--- src/Screens/Generator.js
+++ src/Screens/Generator.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Draggable from "react-draggable";
 import domtoimage from "dom-to-image-more";
 import "../Styles/Generator.css";
 
@@ -10,10 +11,7 @@ const Generator = ({ meme, changeMeme }) => {
       .then((response) => response.json())
       .then((response) =>
         changeMeme(response.data.memes[Math.floor(Math.random() * 100)])
-      )
-      .then(() => {
-        dragEvents();
-      });
+      );
   };
 
   const changeText = (num) => {
@@ -33,57 +31,16 @@ const Generator = ({ meme, changeMeme }) => {
     changeMeme(newMeme);
   };
 
-  const dragEvents = () => {
-    const firstText = document.querySelectorAll(".memeText")[0];
-    const secondText = document.querySelectorAll(".memeText")[1];
-    dragElement(firstText);
-    dragElement(secondText);
-  };
-
-  const dragElement = (elmnt) => {
-    const clicker = (e) => {
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      document.onmousemove = elementDrag;
-    };
-
-    const closeDragElement = () => {
-      document.onmouseup = null;
-      document.onmousemove = null;
-    };
-
-    const elementDrag = (e) => {
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-    };
-
-    let pos1 = 0,
-      pos2 = 0,
-      pos3 = 0,
-      pos4 = 0;
-    elmnt.onmousedown = clicker;
-  };
-
   const reset = () => {
     changeMeme(null);
   };
 
   const download = () => {
     domtoimage
-      .toJpeg(
-        document.querySelector("#memePic"),
-        { quality: 0.95 }
-        /*         { height: meme.height < 450 ? meme.height : 450 },
-        { width: meme.width < 750 ? meme.width : 750 } */
-      )
+      .toJpeg(document.querySelector(".memeDownload"), { quality: 1.0 })
       .then(function (dataUrl) {
-        let link = document.createElement("a");
-        link.download = "my-meme.jpeg";
+        const link = document.createElement("a");
+        link.download = "my-meme.jpg";
         link.href = dataUrl;
         link.click();
       });
@@ -92,25 +49,34 @@ const Generator = ({ meme, changeMeme }) => {
   return (
     <div className="App">
       <div className="templateContainer">
-        <p className="memeText">{memeText[0]}</p>
-        <p className="memeText" id="textBottom">
-          {memeText[1]}
-        </p>
-        <img id="memePic" src={meme.url} unselectable="on" />
-      </div>
-      <div className="card-body">
-        <div className="inputFields">
-          <input
-            onChange={() => {
-              changeText(0);
-            }}
-          ></input>
-          <input
-            onChange={() => {
-              changeText(1);
-            }}
-          ></input>
+        <div
+          className="memeDownload"
+          style={{
+            position: "relative",
+            overflow: "hidden",
+            padding: "0",
+          }}
+        >
+          <Draggable bounds="parent">
+            <p className="memeText">{memeText[0]}</p>
+          </Draggable>
+          <Draggable bounds="parent">
+            <p className="memeText">{memeText[1]}</p>
+          </Draggable>
+          <img id="memePic" src={meme.url} unselectable="on" />
         </div>
+      </div>
+      <div className="inputFields">
+        <input
+          onChange={() => {
+            changeText(0);
+          }}
+        ></input>
+        <input
+          onChange={() => {
+            changeText(1);
+          }}
+        ></input>
       </div>
       <div className="buttons">
         <button className="button" onClick={loadMemes}>
