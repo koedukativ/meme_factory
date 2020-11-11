c3e547cd422ccf90b628e61407682eb9b0a8bb51
Joschka Cesar <cesar@posteo.de>
2020-11-11 09:27:12 +0100

    Refactor Meme Generator for the front page

--- src/Components/MemePreview.js
+++ src/Components/MemePreview.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "../Styles/Select.css";
+import { Link } from "react-router-dom";
 
-function MemePreview(element) {
+const MemePreview = (element) => {
   return (
-    <a href="#" onClick={() => console.log(element)}>
-      <article className="memeContainer">
-        <img src={element.element.url}></img>
-      </article>
-    </a>
+    <article className="memeContainer">
+      <img src={element.element.url}></img>
+    </article>
   );
-}
+};
 
 export default MemePreview;
--- src/Screens/Select.js
+++ src/Screens/Select.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.css";
 import "../Styles/Select.css";
 import MemePreview from "../Components/MemePreview";
+import Generator from "../Screens/Generator";
 
 function Select() {
   const [memes, setMemes] = useState();
+  const [template, setTemplate] = useState();
+
   const loadMemes = () => {
     fetch("https://api.imgflip.com/get_memes")
       .then((response) => response.json())
@@ -18,16 +20,26 @@ function Select() {
   }, []);
 
   return (
-    <section className="selectScreen">
-      {memes
-        ? memes.map((element) => (
-            <MemePreview
-              element={element}
-              onPress={() => console.log("test")}
-            />
+    <>
+      <h1 className="title"> Meme Machine</h1>
+      {template ? <Generator meme={template} changeMeme={setTemplate} /> : null}
+      <section className="selectScreen">
+        {memes ? (
+          memes.map((element) => (
+            <a
+              href="#"
+              onClick={() => {
+                setTemplate(element);
+              }}
+            >
+              <MemePreview element={element} />
+            </a>
           ))
-        : null}
-    </section>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
+    </>
   );
 }
 
